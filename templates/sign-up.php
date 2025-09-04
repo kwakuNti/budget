2b39d2a4251ccf0cc690151284ddd58b8cbf1e86
Clifford Nti Nkansah <159504184+kwakuNti@users.noreply.github.com>
2025-09-04 15:18:04 +0000

    feat: Include favicon in donate and sign-up pages for improved branding

--- templates/sign-up.php
+++ templates/sign-up.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up - Budgetly</title>
+    <?php include '../includes/favicon.php'; ?>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../public/css/loading.css">
     <link rel="stylesheet" href="../public/css/personal.css">
